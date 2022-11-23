d0c887649ded9367e4d4fce172767247e1ef24e4
Marit de Vries <Maritdevries1@live.nl>
2022-11-23 15:55:18 +0100

    Made state on filter not mutate

--- src/reducers/songFilterReducer.js
+++ src/reducers/songFilterReducer.js
@@ -1,11 +1,16 @@
 import { SET_FILTER } from "../actionTypes/actionTypes";
 
-const initialState = "";
+const initialState = {
+    selectedGenre: ""
+};
 
 export const songFilterReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_FILTER:
-            return action.payload
+            return {
+                ...state,
+                selectedGenre: action.payload
+            }
 
         default:
             return state  
